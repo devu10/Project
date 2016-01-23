81abf4111cd529d496bc55b69b1e68d68317c438
devu10 <devudevu10@gmail.com>
2016-01-23 22:16:01 +0545

    first commit

---
+++ SmartTravelCompanion/app/src/main/java/test/com/smarttravelcompanion/maps/test.java
@@ -0,0 +1,18 @@
+package test.com.smarttravelcompanion.maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import test.com.smarttravelcompanion.R;
+
+/**
+ * Created by devu on 1/23/2016.
+ */
+public class test extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
